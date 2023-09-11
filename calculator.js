2d43348b2ccb0839065def18617ba2ac6d9d3622
birddogsix <charliejohnson5of7@gmail.com>
2023-09-11 16:12:31 -0400

    updating for other computer

--- calculator.js
+++ calculator.js
@@ -215,8 +215,9 @@ class Line {
             const currentToken = this.tokens[this.tokenIndex]
 
             if (!levels[0].includes(currentToken.value)) {
-                break   
+                break
             }
+
             this.tokenIndex++
             const operatorNode = new Node(currentToken)
             operatorNode.addChild(leftNode)
@@ -235,161 +236,183 @@ class Line {
 
     evaluate() {
         if (this.input.length == 0) return
-        this.answer = this.#evaluateNode(this.ast)
+        this.answer = this.#evaluateNode(this.ast, ["i"])
         return this.answer
     }
 
-    #evaluateNode(node, returnString) {
+    #evaluateNode(node, varKeeps) {
 
-        // console.log(node)
-        // console.log(returnString)
+        const currentValue = node.value
+        switch (node.type) {
 
-        if (node.type == 'number') {
-            let num = Number(node.value)
-            if (isNaN(num)) {
-                throw new Error("Number \"" + node.value + "\" was not able to be parsed.")
-            }
-            return num
-        } else if (node.type == "variable") {
+            case "number":
+                const num = Number(currentValue)
+                if (isNaN(num)) throw new Error("Number \"" + currentValue + "\" was not able to be parsed.")
+                return num
 
-            let varAnswer = this.variables?.[node.value]
-            // console.log(varAnswer)
-            if (varAnswer != undefined) {
-                return this.variables[node.value]
-            } else {
-                throw new Error("Unknown variable \"" + node.value + "\"")
-            }
+            case "variable":
+                if (varKeeps.includes(currentValue)) return currentValue
+                const varAns = this.variables?.[currentValue]
+                if //todo
+                break
 
-        } else if (node.type == "assignment") {
-
-            const leftNode = node.children[0]
-            if (leftNode.type == "possible variable" || leftNode.type == "variable") {
-
-                // TODO how to assign variables in a whole calculator instead of just in this line
-
-                const rightValue = this.#evaluateNode(node.children[1])
-                this.variables[leftNode.value] = rightValue
-                return rightValue
-
-            } else if (leftNode.type == "possible function" || leftNode.type == "function") {
-
-                // TODO how to assign functions in a whole calculator instead of just in this line
-
-                const functionName = leftNode.value
-
-                // get parameters
-                let functionNodeParameters = leftNode.children
-
-                // get equation for function
-                let functionNodeEquation = this.#evaluateNode(node.children[1], true)
-
-                // check for invalid parameters
-                functionNodeParameters.some((node) => {
-                    const isParameter = ["possible variable", "possible function", "variable", "function"].includes(node.type)
-                    if (isParameter) {
-                        return true
-                    } else {
-                        throw new Error("Invalid parameter \"" + node.value + "\" for function assignment \"" + functionName + "\"")
-                    }
-                })
-
-                let functionParameters = functionNodeParameters.map(parameter => parameter.value)
-
-                const formattedEquation = functionNodeEquation?.type ? this.#formatEquation(functionNodeEquation) : functionNodeEquation
-
-                // console.log("prev", functionNodeEquation)
-                let functionString = `(${functionParameters})=>\`${formattedEquation}\``
-                // console.log("post", functionString)
-
-                this.functions[functionName] = eval(functionString) // TODO: variables and functions arent working here
-
-                return
-
-            } else {
-                throw new Error("Invalid assignment to \"" + leftNode.value + "\"")
-            }
-
-        } else if (node.type == 'operator') {
-
-            const leftValue = this.#evaluateNode(node.children[0], returnString)
-            const rightValue = this.#evaluateNode(node.children[1], returnString)
-            return this.#evaluateOperator(node.value, leftValue, rightValue, returnString)
-
-        } else if (node.type == 'function') {
-            // Handle function evaluation
-            return this.#evaluateFunction(node, returnString)
-        } else if (returnString && node.type == "possible variable") {
-            return node.value
-        } else {
-            throw new Error("Invalid node type \"" + node.type + "\" (\"" + node.value + "\").")
-        }
-
-
-    }
-
-    #evaluateOperator(operator, left, right, returnString) {
-
-        if (returnString) {
-            return left + operator + right
-        }
-
-        switch (operator) {
-            case '+':
-                return left + right
-            case '-':
-                return left - right
-            case '*':
-                return left * right
-            case '/':
-                if (right == 0) {
-                    throw new Error("Division by zero.")
-                }
-                return left / right
-            case '^':
-                return left ** right
             default:
-                throw new Error("Unknown operator \"" + operator + "\"")
+                break
         }
+
     }
 
-    #evaluateFunction(node, returnString) {
-        let childrenAnswers = node.children.map(child => this.#evaluateNode(child, returnString))
-        if (returnString && node.children.some(child => child.type == "possible variable")) {
-            return node.value + `(${childrenAnswers})`
-        }
-        let currentFunction = this.functions?.[node.value]
-        if (!currentFunction) {
-            throw new Error("The function \"" + node.value + "\" does not exist.")
-        }
-        if (currentFunction.length != node.children.length) {
-            throw new Error("Not the correct number of arguments for the function \"" + node.value + "\"")
-        }
-        let formulaText = currentFunction(...childrenAnswers)
-        return eval(formulaText)
-    }
+    // #evaluateNode(node, returnString) {
+
+    //     // console.log(node)
+    //     // console.log(returnString)
+
+    //     if (node.type == 'number') {
+    //         let num = Number(node.value)
+    //         if (isNaN(num)) {
+    //             throw new Error("Number \"" + node.value + "\" was not able to be parsed.")
+    //         }
+    //         return num
+    //     } else if (node.type == "variable") {
+
+    //         let varAnswer = this.variables?.[node.value]
+    //         // console.log(varAnswer)
+    //         if (varAnswer != undefined) {
+    //             return this.variables[node.value]
+    //         } else {
+    //             throw new Error("Unknown variable \"" + node.value + "\"")
+    //         }
+
+    //     } else if (node.type == "assignment") {
+
+    //         const leftNode = node.children[0]
+    //         if (leftNode.type == "possible variable" || leftNode.type == "variable") {
+
+    //             // TODO how to assign variables in a whole calculator instead of just in this line
+
+    //             const rightValue = this.#evaluateNode(node.children[1])
+    //             this.variables[leftNode.value] = rightValue
+    //             return rightValue
+
+    //         } else if (leftNode.type == "possible function" || leftNode.type == "function") {
+
+    //             // TODO how to assign functions in a whole calculator instead of just in this line
+
+    //             const functionName = leftNode.value
+
+    //             // get parameters
+    //             let functionNodeParameters = leftNode.children
+
+    //             // get equation for function
+    //             let functionNodeEquation = this.#evaluateNode(node.children[1], true)
+
+    //             // check for invalid parameters
+    //             functionNodeParameters.some((node) => {
+    //                 const isParameter = ["possible variable", "possible function", "variable", "function"].includes(node.type)
+    //                 if (isParameter) {
+    //                     return true
+    //                 } else {
+    //                     throw new Error("Invalid parameter \"" + node.value + "\" for function assignment \"" + functionName + "\"")
+    //                 }
+    //             })
+
+    //             let functionParameters = functionNodeParameters.map(parameter => parameter.value)
+
+    //             const formattedEquation = functionNodeEquation?.type ? this.#formatEquation(functionNodeEquation) : functionNodeEquation
+
+    //             // console.log("prev", functionNodeEquation)
+    //             let functionString = `(${functionParameters})=>\`${formattedEquation}\``
+    //             // console.log("post", functionString)
+
+    //             this.functions[functionName] = eval(functionString) // TODO: variables and functions arent working here
+
+    //             return
+
+    //         } else {
+    //             throw new Error("Invalid assignment to \"" + leftNode.value + "\"")
+    //         }
+
+    //     } else if (node.type == 'operator') {
+
+    //         const leftValue = this.#evaluateNode(node.children[0], returnString)
+    //         const rightValue = this.#evaluateNode(node.children[1], returnString)
+    //         return this.#evaluateOperator(node.value, leftValue, rightValue, returnString)
+
+    //     } else if (node.type == 'function') {
+    //         // Handle function evaluation
+    //         return this.#evaluateFunction(node, returnString)
+    //     } else if (returnString && node.type == "possible variable") {
+    //         return node.value
+    //     } else {
+    //         throw new Error("Invalid node type \"" + node.type + "\" (\"" + node.value + "\").")
+    //     }
+
+
+    // }
+
+    // #evaluateOperator(operator, left, right, returnString) {
+
+    //     if (returnString) {
+    //         return left + operator + right
+    //     }
+
+    //     switch (operator) {
+    //         case '+':
+    //             return left + right
+    //         case '-':
+    //             return left - right
+    //         case '*':
+    //             return left * right
+    //         case '/':
+    //             if (right == 0) {
+    //                 throw new Error("Division by zero.")
+    //             }
+    //             return left / right
+    //         case '^':
+    //             return left ** right
+    //         default:
+    //             throw new Error("Unknown operator \"" + operator + "\"")
+    //     }
+    // }
+
+    // #evaluateFunction(node, returnString) {
+    //     let childrenAnswers = node.children.map(child => this.#evaluateNode(child, returnString))
+    //     if (returnString && node.children.some(child => child.type == "possible variable")) {
+    //         return node.value + `(${childrenAnswers})`
+    //     }
+    //     let currentFunction = this.functions?.[node.value]
+    //     if (!currentFunction) {
+    //         throw new Error("The function \"" + node.value + "\" does not exist.")
+    //     }
+    //     if (currentFunction.length != node.children.length) {
+    //         throw new Error("Not the correct number of arguments for the function \"" + node.value + "\"")
+    //     }
+    //     let formulaText = currentFunction(...childrenAnswers)
+    //     return eval(formulaText)
+    // }
+
+    // #formatEquation(node) {
+    //     if (node.type == "function") {
+    //         // TODO finish function replacement
+    //         let currentFunc = this.functions[node.value]
+    //         let subParameters = node.children.map(child => this.#formatEquation(child))
+    //         if (subParameters.length != currentFunc.length) {
+    //             throw new Error("Incorrect number of parameters for the function \"" + node.value + "\".")
+    //         }
+    //         return currentFunc(...subParameters)
+    //     } else if (node.type == "operator") {
+    //         let left = this.#formatEquation(node.children[0])
+    //         let right = this.#formatEquation(node.children[1])
+    //         return `${left}${node.value}${right}`
+    //     } else if (node.type == "assignment") {
+    //         throw new Error("Invalid assignment.")
+    //     } else {
+    //         return `\${${node.value}}`
+    //     }
+    // }
 
     getStoredAnswer() {
         return this.answer
-    }
-
-    #formatEquation(node) {
-        if (node.type == "function") {
-            // TODO finish function replacement
-            let currentFunc = this.functions[node.value]
-            let subParameters = node.children.map(child => this.#formatEquation(child))
-            if (subParameters.length != currentFunc.length) {
-                throw new Error("Incorrect number of parameters for the function \"" + node.value + "\".")
-            }
-            return currentFunc(...subParameters)
-        } else if (node.type == "operator") {
-            let left = this.#formatEquation(node.children[0])
-            let right = this.#formatEquation(node.children[1])
-            return `${left}${node.value}${right}`
-        } else if (node.type == "assignment") {
-            throw new Error("Invalid assignment.")
-        } else {
-            return `\${${node.value}}`
-        }
     }
 
 }
