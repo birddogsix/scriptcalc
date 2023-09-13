6f3c0c8685467cdedf9399f18c3f7cab46a1af31
birddogsix <charliejohnson5of7@gmail.com>
2023-09-12 23:10:55 -0400

    updated look and fixed exponent bug

--- notepad.js
+++ notepad.js
@@ -2,17 +2,24 @@ const lineNumbers = document.querySelector("#lineNumbers")
 const calcAnswers = document.querySelector("#calcAnswers")
 const codeText = document.querySelector("#codeText")
 
-// function updateLines(event) {
-    // const linesHTML = event.target.value.split("\n").map((_, i) => i < 10 ? " " + (i + 1) : i + 1).join("<br>")
-    // lineNumbers.innerHTML = linesHTML
-// }
+function updateLineNumbers(target) {
+    const linesHTML = target.value.split("\n").map((_, i) => i + 1).join("\n")
+    lineNumbers.innerHTML = linesHTML
+    updatePositions()
+}
 
 function updateCalculations(target) {
     text = target.value
-    const calculator = new Calculator(text)
-    let answersHTML = calculator.solveAll().join("<br>")
-    // calculator.lines.forEach(l=>console.log(l.tokens,l.ast,l.variables,l.functions))
+    const calculator = new Calculator(text, true)
+    let answersHTML = calculator.solveAll().join("\n")
+    calculator.lines.forEach(l=>console.log(l.tokens,l.ast,l.variables,l.functions,`${l.functions.f}`))
     calcAnswers.innerHTML = answersHTML
 }
 
+function updatePositions() {
+    lineNumbers.scrollTop = codeText.scrollTop
+    calcAnswers.scrollTop = codeText.scrollTop
+}
+
 updateCalculations(codeText)
+updateLineNumbers(codeText)
